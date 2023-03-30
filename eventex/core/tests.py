2d13ef300b4f59d6a1737d552b78a1b26e6eb147
Maisa Preis <maisapierinipreis@gmail.com>
2023-03-30 19:39:09 -0300

    Os teste foram criados.

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Um 'assert' por método de teste.
+#Rodar teste: manage test
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
