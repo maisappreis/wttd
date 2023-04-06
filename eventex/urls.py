5d1b83b6cbb7e73f8e494490e034c28d3f4ff65d
Maisa Preis <maisapierinipreis@gmail.com>
2023-04-06 20:11:10 -0300

    Teste

--- eventex/urls.py
+++ eventex/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 import eventex.core.views
 from eventex.subscriptions.views import subscribe
 
+
 urlpatterns = [
     path('', eventex.core.views.home),
     path('inscricao/', subscribe),
