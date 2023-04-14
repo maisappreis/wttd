bb288d23fcfbd099c5ae2fbd3d6a3bc71eed176c
Maisa Preis <maisapierinipreis@gmail.com>
2023-04-14 18:28:52 -0300

    Email and Phone are optional.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -15,8 +15,8 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='E-mail')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='E-mail', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     # Esse é um método especial, basta chamar o 'clean' mais o '_' e mais o nome do campo.
     # Quando implementar esse método, é necessário retornar o valor correto, porque ele vai substituir o 'cleaned_data['name']'
@@ -31,5 +31,11 @@ class SubscriptionForm(forms.Form):
         # words = [w.capitalized() for w in name.split()]
 
         capitalized_name = ' '.join(words)
-
         return capitalized_name
+    
+    # Esse é o 'clean' do formulário como um todo, ele é chamado depois de todos os outros 'clean'.
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        
+        return self.cleaned_data # precisa retornar os dados, mesmo que não haja nenhuma modificação.
